4f4f13422a1b5ae3b74139025c9468e83536c6fc
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2026-02-02 19:54:50 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+interface IMovie {
+  title: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState<IMovie[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/movie/movies")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setLoading(false);
+        setMovies(data);
+      });
+  }, []);
+
+  if (loading) {
+    return <h1>Unshij bn</h1>;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {movies.map(movie => {
+        return <h1>{movie.title}</h1>;
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
