9f06812e1f0056035b39689ce88307773e3035ca
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2026-02-09 20:17:29 +0800

    movie

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,12 +1,24 @@
-import { Routes, Route } from "react-router";
-import { MainLayout } from "./modules/app/MainLayout";
+import { Routes, Route, Navigate } from "react-router";
+import { MainLayout } from "@/modules/app/MainLayout";
+import { Home } from "@/modules/home/components/Home";
+import { TopMovies } from "./modules/movie/components/TopMovies";
+import { PopularMovies } from "./modules/movie/components/PopularMovies";
+import { AllMovies } from "./modules/movie/components/AllMovie";
+import { MovieDetails } from "./modules/movie/components/details/MovieDetails";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<div>123</div>} />
-        <Route path="about" element={<div>about</div>} />
+      <Route element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies">
+          <Route index element={<Navigate to="all" replace />} />
+          <Route path="top250" element={<TopMovies />} />
+          <Route path="popular" element={<PopularMovies />} />
+          <Route path="all" element={<AllMovies />} />
+
+          <Route path="details/:id" element={<MovieDetails />} />
+        </Route>
       </Route>
     </Routes>
   );
